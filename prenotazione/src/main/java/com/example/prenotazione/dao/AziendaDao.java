f18e97dc9a8d7e41e0e5463f8c9036e8fee478ec
alessiociaccia <95575782+alessiociaccia@users.noreply.github.com>
2021-12-06 11:08:39 +0100

    zexrtyui,

---
+++ prenotazione/src/main/java/com/example/prenotazione/dao/AziendaDao.java
@@ -0,0 +1,9 @@
+package com.example.prenotazione.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.prenotazione.model.Azienda;
+
+public interface AziendaDao extends CrudRepository<Azienda, Integer>{
+
+}
