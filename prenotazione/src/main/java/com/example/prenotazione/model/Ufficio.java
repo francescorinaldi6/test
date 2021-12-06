3de0921dfff65db67ba95230b4d17a0ae3d33018
alessiociaccia <95575782+alessiociaccia@users.noreply.github.com>
2021-12-06 11:31:48 +0100

    Aggiunta entità ufficio

---
+++ prenotazione/src/main/java/com/example/prenotazione/model/Ufficio.java
@@ -0,0 +1,29 @@
+package com.example.prenotazione.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="azienda")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+
+public class Ufficio {
+	@Id @GeneratedValue
+	public int id_ufficio;
+	public String indirizzo;
+	public int grandezza;
+	public int id_azienda;
+}
