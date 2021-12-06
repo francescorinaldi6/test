7ec8deada3b61e70ce674503b28ec8fc02ea4ff4
alessiociaccia <95575782+alessiociaccia@users.noreply.github.com>
2021-12-06 10:38:00 +0100

    Revert "primoCommit"
    This reverts commit 91498ff5ede43af2ffe59adeec5a1373e1349fab.

--- prenotazione/src/main/java/com/example/prenotazione/dao/AziendaDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.prenotazione.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.prenotazione.model.Azienda;
-
-public interface AziendaDao extends CrudRepository<Azienda, Integer>{
-
-}
