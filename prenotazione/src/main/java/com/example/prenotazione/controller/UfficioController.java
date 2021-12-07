29fdafdb6405a7f63df58cfc7467299c4c5bfe8b
alessiociaccia <95575782+alessiociaccia@users.noreply.github.com>
2021-12-07 11:43:26 +0100

    aggiunta ufficiocontroller

---
+++ prenotazione/src/main/java/com/example/prenotazione/controller/UfficioController.java
@@ -0,0 +1,34 @@
+package com.example.prenotazione.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.prenotazione.dao.UfficioDao;
+import com.example.prenotazione.model.Ufficio;
+
+@RestController
+@RequestMapping("/Ufficio")
+
+public class UfficioController {
+	@Autowired
+	private UfficioDao dao;
+	
+	@PostMapping("/addUffici")
+	public String addUffici(@RequestBody List<Ufficio> uffici) {
+		
+		for (int i=0; i<uffici.size(); i++)
+		dao.save(uffici.get(i));
+		return  "Sono stati aggiunti " + uffici.size() + " uffici";
+	}
+	
+	@GetMapping("/getUffici")
+	public List<Ufficio> getUffici() {
+		return (List<Ufficio>) dao.findAll();
+	}
+}
