51c053aec7e292d218b7dc444a6af5c91a68f87c
francy1601 <39488147+francy1601@users.noreply.github.com>
2021-12-07 10:16:25 +0100

    Add UtenteController SignUp

---
+++ prenotazione/src/main/java/com/example/prenotazione/controller/UtenteController.java
@@ -0,0 +1,31 @@
+package com.example.prenotazione.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.prenotazione.dao.UtenteDao;
+import com.example.prenotazione.model.Utente;
+
+@RestController
+@RequestMapping("/Utente")
+
+public class UtenteController {
+
+	@Autowired
+	private UtenteDao dao;
+	
+	@PostMapping("/signup")
+	public String addUtente(@RequestBody Utente utente) {
+		
+		
+		dao.save(utente);
+		return  "Utente aggiunto";
+	}
+	
+	
+}
--- prenotazione/src/main/java/com/example/prenotazione/model/Utente.java
+++ prenotazione/src/main/java/com/example/prenotazione/model/Utente.java
@@ -26,7 +26,7 @@ public class Utente {
 	public String nome;
 	public String cognome;
 	public String e_mail;
-	public int numero;  //vedere se cambiare in String visto che un numero a 10 cifre è pesante
+	public String numero;  //vedere se cambiare in String visto che un numero a 10 cifre è pesante
 	public int id_azienda;
 	
 }
