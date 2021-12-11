7d9fa67ddf758c17cd2a82c0b675baf6c52837dc
alessiociaccia <95575782+alessiociaccia@users.noreply.github.com>
2021-12-11 15:53:42 +0100

    asd

--- prenotazione/src/main/java/com/example/prenotazione/dao/UtenteDao.java
+++ prenotazione/src/main/java/com/example/prenotazione/dao/UtenteDao.java
@@ -13,11 +13,4 @@ public interface UtenteDao extends CrudRepository<Utente, Integer>{
 	//SELECT * FROM prenotazione.auth_user_role INNER JOIN prenotazione.utente ON prenotazione.auth_user_role.auth_user_id=prenotazione.utente.id_utente
 	@Query(value="SELECT id_azienda FROM azienda a WHERE a.id_azienda = :id", nativeQuery = true) List<Integer> aziendaExists(@Param("id") int id);
 	
-	
-<<<<<<< HEAD
-	@Query(value="SELECT e_mail FROM utente u WHERE u.e_mail = :e_mail", nativeQuery = true) List<Integer> e_mailExists(@Param("e_mail") String e_mail);
-=======
-//	@Query(value="SELECT e_mail FROM utente u WHERE u.e_mail = :mail", nativeQuery = true) List<Integer> e_mailExists(@Param("mail") String e_mail);
->>>>>>> branch 'main' of git@github.com:francescorinaldi6/test.git
-	
 }
