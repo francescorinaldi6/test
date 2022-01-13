40000db873ef4812ec1a6fad5a07685195ee065b
francy1601 <39488147+francy1601@users.noreply.github.com>
2022-01-13 16:14:42 +0100

    crossOrigin

--- prenotazione/src/main/java/com/example/prenotazione/controller/PrenotaController.java
+++ prenotazione/src/main/java/com/example/prenotazione/controller/PrenotaController.java
@@ -13,6 +13,7 @@ import javax.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,7 +37,7 @@ import com.example.prenotazione.service.QRCodeGenerator;
 import com.example.prenotazione.service.ServiceForgotPassword;
 import com.example.prenotazione.service.ServiceSendQr;
 import com.google.zxing.WriterException;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/Prenota")
 
