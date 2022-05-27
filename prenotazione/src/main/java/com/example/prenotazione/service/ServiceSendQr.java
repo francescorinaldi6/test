f542b1a1b424eef77fbbf80a87fd164fa8e5da54
alessiociaccia <95575782+alessiociaccia@users.noreply.github.com>
2022-05-27 10:26:33 +0200

    asd

--- prenotazione/src/main/java/com/example/prenotazione/service/ServiceSendQr.java
+++ prenotazione/src/main/java/com/example/prenotazione/service/ServiceSendQr.java
@@ -55,12 +55,12 @@ import javax.mail.Message;
 					helper.setSubject("Qr Prenotazione"); 
 					
 					//helper.setText(text);    //dare info sulla prenotazione passando parametri
-					helper.setText("<html> <body><b>PRENOTAZIONE </b>   </body> <form>\r\n"
-							+ "    <a href=\""+UrlCalendar+"\">\r\n"
-							+ "        <input type=\"button\" value=\"Clicca quì \" style=\"border-radius: 10px 10px 10px 10px; background-color: rgb(27, 30, 230); color: #fff;\">\r\n"
-							+ "    </a>\r\n"
-							+ " <a>per aggiungere l'evento al tuo calendario </a>\r\n"
-							+ "</form> </html> ",true);
+					helper.setText("<html> <body><b>PRENOTAZIONE </b>   </body> <form>\r\n" 
+						       + "    <a href=\""+UrlCalendar+"\">\r\n" 
+						       + "        <input type=\"button\" value=\"Clicca qui \" style=\"border-radius: 10px 10px 10px 10px; background-color: rgb(27, 30, 230); color: #fff;\">\r\n" 
+						       + "    </a>\r\n" 
+						       + " <a>per aggiungere l'evento al tuo calendario </a>\r\n" 
+						       + "</form> </html> ",true);
 					
 						
 					FileSystemResource file = new FileSystemResource("../prenotazione/QrCode.jpg");
